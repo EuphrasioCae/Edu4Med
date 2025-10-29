5b2cf2c766b0bed494479c6fd4d5600ae5d515b4
Caê <134336937+EuphrasioCae@users.noreply.github.com>
2025-10-29 09:01:03 -0300

    Delete src directory

--- src/components/constants.ts
+++
@@ -1,7 +0,0 @@
-export const ESTADOS_BRASIL = [
-    { value: 'Norte.', label: 'Norte' },
-    { value: 'Nordeste.', label: 'Nordeste' },
-    { value: 'Centro-Oeste.', label: 'Centro-Oeste' },
-    { value: 'Sudeste.', label: 'Sudeste' },
-    { value: 'Sul.', label: 'Sul' }
-  ];
